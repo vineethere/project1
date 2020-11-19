c88850d503ae88a55ced484e00a1fb1df923f7f7
Vineet <vineetarora2001@gmail.com>
2020-11-19 23:47:58 +0530

    constructor_added

---
+++ constructor.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class vineet{
+	public:
+      vineet();
+ 
+};
+     vineet :: vineet()
+    {
+    cout<<"vineet";
+	}
+
+
+
+int main()
+{
+	
+	vineet obj1();
+	return 0;
+}
